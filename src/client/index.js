9f36f62d75418b5e866226505436accbb06d2c26
ikatyang <ikatyang@gmail.com>
2017-08-03 22:52:15 +0800

    feat(template): support RWD

---
+++ src/client/index.js
@@ -0,0 +1,56 @@
+import { Scene, WebGLRenderer, GridHelper, PerspectiveCamera } from 'three';
+import OrbitControls from 'orbit-controls-es6';
+
+import $ from 'jquery';
+
+/* eslint-disable no-use-before-define */
+
+const scene = new Scene();
+const canvas = document.getElementById('canvas');
+const renderer = new WebGLRenderer({ canvas });
+const camera = new PerspectiveCamera();
+const controls = new OrbitControls(camera, renderer.domElement);
+
+init();
+animate();
+
+function init() {
+  camera.position.set(0, 200, 100);
+  renderer.setClearColor(0x888888);
+
+  const gridXZ = new GridHelper(100, 20, 0xff0000, 0xffffff);
+  scene.add(gridXZ);
+
+  $(onWindowResize);
+  $(window).resize(onWindowResize);
+}
+
+function animate() {
+  controls.update();
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+let isSideControls = true;
+const $controls = $('#controls');
+const $sideControls = $('#side-controls');
+const $navbarControls = $('#navbar-controls');
+
+function onWindowResize() {
+  const $parent = $(canvas).parent();
+  const width = $parent.innerWidth();
+  const height = $parent.innerHeight();
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+
+  if (window.innerWidth < 768) {
+    if (isSideControls) {
+      isSideControls = false;
+      $navbarControls.append($controls.detach());
+    }
+  } else if (!isSideControls) {
+    isSideControls = true;
+    $sideControls.append($controls.detach());
+  }
+}
